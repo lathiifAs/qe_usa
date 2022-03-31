b340a6e2391e2f8cef2b85c048109fdc7af43cec
lathiifAs <ajisanthoshol@gmail.com>
2022-03-31 13:55:30 +0700

    a

--- resources/views/quality_express/detail.blade.php
+++ resources/views/quality_express/detail.blade.php
@@ -25,7 +25,7 @@
         <font size="2">
         <table border="0" style="border-color: black; border-collapse: collapse; width:100%; margin-top: 5%;" cellpadding="0" cellspacing="0" >
             <tr>
-                <td rowspan="2" style="width: 35%">
+                <td rowspan="2" style="width: 45%">
                     <p>
                         {{ $qe->shipper }}
                     </p>
@@ -67,7 +67,7 @@
                     <p>
                     </p>
                 </td>
-                <td colspan style="width: 35%">
+                <td colspan style="width: 45%">
                     {{ $qe->issued_by }}
                 </td>
             </tr>
